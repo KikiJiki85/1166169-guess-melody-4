e3a1a42f3c3d9dac52ef4c6d6fd6057ad970ad45
Aleksey Vaskin <alzor85@gmail.com>
2020-06-23 09:27:36 +0300

    Добавит компонент: Вопрос по жанру

---
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -0,0 +1,64 @@
+import React, {PureComponent} from "react";
+import PropTypes from "prop-types";
+import {GameType} from "../../const.js";
+
+class GenreQuestionScreen extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      answers: [false, false, false, false],
+    };
+
+  }
+
+  render() {
+    const {onAnswer, question} = this.props;
+    const {answers: userAnswers} = this.state;
+    const {
+      answers,
+      genre,
+    } = question;
+
+    return (
+      <section className="game game--genre">
+        <header className="game__header">
+          <a className="game__back" href="#">
+            <span className="visually-hidden">Сыграть ещё раз</span>
+            <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"/>
+          </a>
+
+          <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+            <circle className="timer__line" cx="390" cy="390" r="370"
+              style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
+          </svg>
+
+          <div className="game__mistakes">
+            <div className="wrong"/>
+            <div className="wrong"/>
+            <div className="wrong"/>
+          </div>
+        </header>
+
+        <section className="game__screen">
+          <h2 className="game__title">Выберите {genre} треки</h2>
+        </section>
+      </section>
+    );
+  }
+}
+
+GenreQuestionScreen.propTypes = {
+  onAnswer: PropTypes.func.isRequired,
+  question: PropTypes.shape({
+    answers: PropTypes.arrayOf(PropTypes.shape({
+      src: PropTypes.string.isRequired,
+      genre: PropTypes.string.isRequired,
+    })).isRequired,
+    genre: PropTypes.string.isRequired,
+    type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired,
+  }).isRequired,
+};
+
+
+export default GenreQuestionScreen;
