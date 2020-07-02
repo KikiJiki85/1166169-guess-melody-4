b5ab7dd4dd851beed423c78446f1c646c57fe91d
Aleksey Vaskin <alzor85@gmail.com>
2020-07-02 16:47:29 +0300

    Добавит компонент GameScreen. Перенесёт общую разметку

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,14 @@
 import React, {PureComponent} from "react";
+import PropTypes from "prop-types";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
-import PropTypes from "prop-types";
 import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
+import GameScreen from "../game-screen/game-screen.jsx";
 import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
 import {GameType} from "../../const.js";
 
-export default class App extends PureComponent {
+
+class App extends PureComponent {
   constructor(props) {
     super(props);
 
@@ -19,6 +21,7 @@ export default class App extends PureComponent {
     const {errorsCount, questions} = this.props;
     const {step} = this.state;
     const question = questions[step];
+
     if (step === -1 || step >= questions.length) {
       return (
         <WelcomeScreen
@@ -36,34 +39,43 @@ export default class App extends PureComponent {
       switch (question.type) {
         case GameType.ARTIST:
           return (
-            <ArtistQuestionScreen
-              question={question}
-              onAnswer={() => {
-                this.setState((prevState) => ({
-                  step: prevState.step + 1,
-                }));
-              }}
-            />
+            <GameScreen
+              type={question.type}
+            >
+              <ArtistQuestionScreen
+                question={question}
+                onAnswer={() => {
+                  this.setState((prevState) => ({
+                    step: prevState.step + 1,
+                  }));
+                }}
+              />
+            </GameScreen>
           );
         case GameType.GENRE:
           return (
-            <GenreQuestionScreen
-              question={question}
-              onAnswer={() => {
-                this.setState((prevState) => ({
-                  step: prevState.step + 1,
-                }));
-              }}
-            />
+            <GameScreen
+              type={question.type}
+            >
+              <GenreQuestionScreen
+                question={question}
+                onAnswer={() => {
+                  this.setState((prevState) => ({
+                    step: prevState.step + 1,
+                  }));
+                }}
+              />
+            </GameScreen>
           );
       }
     }
+
     return null;
   }
 
   render() {
-
     const {questions} = this.props;
+
     return (
       <BrowserRouter>
         <Switch>
@@ -90,5 +102,8 @@ export default class App extends PureComponent {
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
-  questions: PropTypes.array.isRequired
+  questions: PropTypes.array.isRequired,
 };
+
+
+export default App;
